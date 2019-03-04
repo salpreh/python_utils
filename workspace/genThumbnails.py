5e3b0dd4bd1554999131b00b2a4d08e34b33175c
Salva Pérez <salvaperez@digitalatl.com>
2019-03-04 07:58:49 +0100

    Thumb generator script refactored

--- workspace/genThumbnails.py
+++ workspace/genThumbnails.py
@@ -8,31 +8,19 @@ from pathlib import Path
 THUMB_SIZE = 420
 VALID_EXTENSIONS = ['.jpg', '.jpeg', '.JPG', '.JPEG']
 
-if __name__ == '__main__':
 
-    # User inputs
-    in_folder = input('Path to images folder: ')
-    in_folder_path = Path(in_folder)
-    if not in_folder_path.exists() or not in_folder_path.is_dir():
-        print("The folder not found: {}".format(in_folder_path.resolve()))
-        sys.exit(0)
-
-    out_folder = input('Folder to save thumbs (optional): ')
-    if not out_folder:
-        out_folder_path = in_folder_path / 'thumbs'
-
-    if not out_folder_path.exists():
-        out_folder_path.mkdir(parents=True)
-
+def genThumbs(input_folder, thumb_folder, valid_ext=VALID_EXTENSIONS):
     # Get images in folder and process it
-    images = [f for f in in_folder_path.iterdir() if f.suffix in VALID_EXTENSIONS]
+    images = [f for f in input_folder.iterdir() if f.suffix in valid_ext]
     if images:
         print('>> Procesing images...')
     else:
-        print('>> Images not found. Valid extensions: {}'.format(VALID_EXTENSIONS))
+        print('>> Images not found. Valid extensions: {}'.format(valid_ext))
     for image in images:
         try:
             im = Image.open(image.resolve())
+            if image.suffix == '.png':
+                im = im.convert('RGB')
 
             # Check proportions and calculate final size
             orWidth, orHeight = im.size
@@ -46,11 +34,31 @@ if __name__ == '__main__':
 
             # Resize and save
             thumb_img = im.resize((thWidth, thHeight), Image.ANTIALIAS)
-            thumb_path = out_folder_path / '{}.{}'.format(image.stem, 'jpg')
+            thumb_path = thumb_folder / '{}.{}'.format(image.stem, 'jpg')
             thumb_img.save(thumb_path.resolve())
             print("> Image '{}' thumb generated".format(image.name))
 
         except IOError:
             print("> Error generating thumb for image '{}'".format(image.name))
+
+
+if __name__ == '__main__':
+
+    # User inputs
+    in_folder = input('Path to images folder: ')
+    in_folder_path = Path(in_folder)
+    if not in_folder_path.exists() or not in_folder_path.is_dir():
+        print("The folder not found: {}".format(in_folder_path.resolve()))
+        sys.exit(0)
+
+    out_folder = input('Folder to save thumbs (optional): ')
+    if not out_folder:
+        out_folder = str(in_folder_path.resolve()) / 'thumbs'
+
+    out_folder_path = Path(out_folder)
+    if not out_folder_path.exists():
+        out_folder_path.mkdir(parents=True)
+
+    genThumbs(in_folder_path, out_folder_path)
 
     print('>> Execution finished')
